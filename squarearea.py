b77ab825d5b431ff88f0294e2c029aa6d409cb08
DINE_J <dinejridi@gmail.com>
2020-07-01 11:59:44 +0200

    add GNU License

--- squarearea.py
+++ squarearea.py
@@ -1,3 +1,20 @@
+"""Copyright (C) 2020 Jridi Dine
+
+This checkers game is a free; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3 of the License, or (at your
+option) any later version.
+
+This checkers game is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this checkers game ; see the file LICENSE.  If not, write to the Free
+Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA."""
+
+
 from gi.repository import Gtk, Gdk, cairo, Pango, PangoCairo
 from gi.repository import GObject
 import math
